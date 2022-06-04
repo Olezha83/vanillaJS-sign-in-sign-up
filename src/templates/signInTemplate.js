e12b4a93133c6054bb31bfdfe34d3ebb907179b2
Oleg <oleg.it.workbox@gmail.com>
2022-06-04 15:29:53 +0300

    markup for sign in callbacks added

--- src/templates/signInTemplate.js
+++ src/templates/signInTemplate.js
@@ -2,11 +2,24 @@ export const signInTemplate = `
   <div class="wrapper">
     <p class="field-heading">Enter your login</p>
     <input id="login" />
+    <div id="login-error">
+      <p>Such user doesn't exist</p>
+    </div>
+    
     <p class="field-heading">Enter your password</p>
     <input id="password" type="password" />
+    <div id="password-error">
+      <p>Incorrect password</p>
+    </div>
+    
     <div id="button-paragraph">
       <button id="submit">Sign in</button>
-    </div>  
+    </div>
+    
+    <div id="congrats">
+      <p>Congratulations!</p>
+      <p>You are now logged in!</p>
+    </div>
   </div>
 
   <style>
@@ -24,6 +37,29 @@ export const signInTemplate = `
     #button-paragraph {
       text-align: center;
       margin: 16px 0 4px;
+    }
+
+    #login-error, #password-error, #congrats {
+      display: none;
+      border: 1px solid #f00;
+      margin: 4px 0;
+      background: #f004;
+    }
+
+    #login-error>p, #password-error>p, #congrats>p {
+      text-align: center;
+      margin: 0;
+      font-style: italic;
+    }
+
+    #congrats {
+      margin-top: 8px;
+      border: 1px solid #090;
+      background: #0904;
+    }
+
+    #congrats>p:first-child {
+      text-transform: uppercase;
     }
     
     .field-heading {
